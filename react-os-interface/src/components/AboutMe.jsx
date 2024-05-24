1bfd6107315d2b58b853ec89a677cd29244ba1ef
WafaBenyettou <benwafa951@gmail.com>
2024-05-24 15:57:45 +0100

    resp

--- react-os-interface/src/components/AboutMe.jsx
+++ react-os-interface/src/components/AboutMe.jsx
@@ -7,7 +7,7 @@ function AboutMe() {
     <div className="p-4" style={{ fontFamily: 'monospace' }}>
       <h2 className="text-2xl font-bold mb-4 text-white">About Me</h2>
       <div className="flex flex-col items-center bg-gray-800 p-4 rounded-lg relative overflow-hidden md:flex-row md:items-start">
-        <img src={AboutImage} alt="Wafa Nesrine BENYETTOU" className="w-40 h-40 mb-4 md:mb-0 md:mr-10 rounded-lg border-2 border-gray-300" />
+        <img src={AboutImage} alt="Wafa Nesrine BENYETTOU" className="z-[999] w-40 h-40 mb-4 md:mb-0 md:mr-10 rounded-lg border-2 border-gray-300" />
         <div className="flex flex-col justify-center">
           <h1 className="text-pink-400 text-2xl mb-2"><strong>whoami?</strong></h1>
           <p className="text-white">Hello! I'm Wafa Nesrine BENYETTOU, a passionate developer with a strong background in information systems security. I love creating innovative solutions and exploring new technologies.</p>
